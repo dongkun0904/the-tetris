45c70b644e3f542154dac9372e24570c4093271b
DongHyun <dongkun0904@gmail.com>
2020-08-13 23:19:35 +0900

    Added network module for communicating [Network]

---
+++ thetetris/network/client.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+
+import socket
+
+
+class Client:
+
+    def __init__(self, server_address, req_code):
+        # Global Variable
+        self.BUFFER_SIZE = 1024
+        self.server_address = server_address
+        self.req_code = req_code
+        self.neg_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.keepPlaying = True
+
+        # Validate
+        try:
+            int(self.req_code)  # just check if req_code is int
+        except ValueError:
+            print("n_port and req_code must be integers")
+            exit()
+
+    def client_negotiation(self):
+        """
+        client_negotiation: str, int, int -> int
+
+        client_negotiation asks the server for a port to talk to using UDP.
+        If the verification is successful, it returns the port number.
+        If the verification is unsuccessful, it returns 0.
+        """
+
+        # Check if the server is up
+        try:
+            self.neg_socket.connect((self.server_address, 54241))
+        except socket.error as e:
+            if e.errno == 111:
+                print("Cannot connect to the server.")
+                print("Make sure the server is running and you entered"
+                      + " correct server address and n_port.")
+                self.neg_socket.close()
+                exit()
+
+        # Send the request code for verification and get the response
+        code = str(self.req_code)
+        self.neg_socket.send(code.encode("utf-8"))
+        resp = self.neg_socket.recv(self.BUFFER_SIZE)
+        resp = int(resp.decode("utf-8"))
+
+        # If the request code is incorrect
+        if resp == 1:
+            print("req code not roccet")
+
+        return resp
+
+    def client_transaction(self, msg):
+        """
+        client_transaction: str, int, str -> None
+
+        client_transaction sends the string to be reversed
+        and get the reversed string from the server using UDP.
+        It also prints the retrieved reversed string.
+        """
+
+        self.neg_socket.send(msg)
+        resp = self.neg_socket.recv(self.BUFFER_SIZE)
+
+        return resp
+
+
+def connect_client(grid, queue):
+    o = Client('219.240.136.145', 13)
+
+    # Negotiation step
+    resp = o.client_negotiation()
+    if resp != 0:
+        # Authentication failed
+        print("authentication failed")
+        exit()
+
+    # Transaction step
+    while o.keepPlaying:
+        queue.put(list(o.client_transaction(grid)))
+    # o.neg_socket.shutdown(socket.SHUT_RDWR)
+    o.neg_socket.close()
+
+
+# if __name__ == "__main__":
+#     connect_client(bytearray([0] * 10))
---
+++ thetetris/network/server.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+
+import socket
+
+
+class Server:
+
+    def __init__(self, req_code):
+        # Global variable
+        self.BUFFER_SIZE = 1024
+        self.server_address = ""
+        self.playing = True
+        self.req_code = req_code
+        self.conn = None
+        self.neg_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # Validate arguments
+        try:
+            int(self.req_code)
+        except ValueError:
+            print("req_code must be an integer")
+            exit()
+
+        # Bind with port number 54241 (or any forwarded port)
+        self.neg_socket.bind((self.server_address, 54241))
+
+    def server_negotiation(self):
+
+        # Accept connection with a client and get the code
+        conn, _ = self.neg_socket.accept()
+        self.conn = conn
+        code = self.conn.recv(self.BUFFER_SIZE)
+
+        # Confirm code is int
+        try:
+            code = code.decode("utf-8")
+            code = int(code)
+        except:
+            print("code from client must be an integer")
+            return False
+
+        # Verify if the client code is acceptable
+        if self.req_code == code:
+            # Accept the request
+            self.conn.send("0".encode("utf-8"))
+            return True
+
+        # Client failed to provide correct code
+        else:
+            self.conn.send("1".encode("utf-8"))
+            self.conn.close()
+        return False
+
+    def server_transaction(self, msg):
+        """
+        server_negotiation: socket.socket -> None
+
+        server_negotiation retrieves a string from a verified client
+        to reverse the string and send it back to the client.
+        """
+
+        opponent_grid = self.conn.recv(self.BUFFER_SIZE)
+
+        # Check if the client replied
+        if not opponent_grid:
+            print("connection lost")
+            self.playing = False
+            return
+
+        self.conn.send(msg)
+
+        return opponent_grid
+
+
+def start_server(grid, queue):
+
+    o = Server(13)
+
+    while True:
+        # Wait for a client to connect
+        print("wait for a client to connect")
+        o.neg_socket.listen(1)
+
+        # Perform the negotiation step
+        established = o.server_negotiation()
+
+        # Exit here if server needs to shut down after incorrect req_code
+
+        if not established:
+            print("req_code is incorrect")
+            continue
+
+        print("connection established")
+        while o.playing:
+            # Perform the transaction step
+            queue.put(list(o.server_transaction(grid)))
+
+        o.conn.close()
+        o.playing = True  # Reset the playing value
+
+
+# if __name__ == "__main__":
+#     o = Server(13)
+#     try:
+#         start_server(o, bytearray([0] * 10))
+#     except KeyboardInterrupt:
+#         o.neg_socket.close()
